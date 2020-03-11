0dc142ee37fbedadadc2e17f70921276d7968eac
cruzortiz99 <cruzortiz099@gmail.com>
2020-03-11 09:02:43 -0400

    updating

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 
 APP.get('/', (request: Request, response: Response) => {
     response.setHeader('Content-Type', 'text/html')
-    response.send('<h2>Hola mundo!</h2>')
+    response.send('<h1>Hola mundo!</h1>')
 })
 
 APP.listen(PORT, () => {
